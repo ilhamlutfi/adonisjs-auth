5eaa1e6cab90a0128c8f4ff9ea2356a3ab6c849f
ilhamlutfi <ilhamlutfi153@gmail.com>
2025-01-28 23:47:33 +0700

    add pages login, register, reset

--- start/routes.ts
+++ start/routes.ts
@@ -10,3 +10,15 @@
 import router from '@adonisjs/core/services/router'
 
 router.on('/').render('pages/home')
+
+router.get('login', ({ view }) => {
+  return view.render('pages/auth/login')
+}).as('login')
+
+router.get('register', ({ view }) => {
+  return view.render('pages/auth/register')
+}).as('register')
+
+router.get('reset-password', ({ view }) => {
+  return view.render('pages/auth/password/reset')
+}).as('reset-password')
