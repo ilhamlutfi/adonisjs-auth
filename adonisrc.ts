fb1c1435f93b76bd4087c0a0a97abcdcee2d8551
ilhamlutfi <ilhamlutfi153@gmail.com>
2025-01-30 15:02:22 +0700

    add rate limiter

--- adonisrc.ts
+++ adonisrc.ts
@@ -37,7 +37,8 @@ export default defineConfig({
     () => import('@adonisjs/lucid/database_provider'),
     () => import('@adonisjs/auth/auth_provider'),
     () => import('@adonisjs/core/providers/vinejs_provider'),
-    () => import('@adonisjs/mail/mail_provider')
+    () => import('@adonisjs/mail/mail_provider'),
+    () => import('@adonisjs/limiter/limiter_provider')
   ],
 
   /*
--- start/routes.ts
+++ start/routes.ts
@@ -14,20 +14,21 @@ import LoginController from '#controllers/auth/login_controller'
 import LogoutsController from '#controllers/auth/logouts_controller'
 import ResetPasswordsController from '#controllers/auth/reset_passwords_controller'
 import ForgotPasswordsController from '#controllers/auth/forgot_passwords_controller'
+import { throttle } from '#start/limiter'
 
 router.on('/').render('pages/home')
 
 router.get('login', [LoginController, 'index']).as('login').use(middleware.guest())
-router.post('login', [LoginController, 'attempt']).as('login.attempt')
-
 router.get('register', [RegistersController, 'index']).as('register')
-router.post('register', [RegistersController, 'store']).as('register.store')
-
 router.get('reset-password', [ResetPasswordsController, 'index']).as('reset-password')
-router.post('reset-password', [ResetPasswordsController, 'attempt']).as('reset-password.attempt')
-
 router.get('forgot-password', [ForgotPasswordsController, 'index']).as('forgot-password')
-router.post('forgot-password', [ForgotPasswordsController, 'attempt']).as('forgot-password.attempt')
+
+router.group(() => {
+  router.post('login', [LoginController, 'attempt']).as('login.attempt')
+  router.post('register', [RegistersController, 'store']).as('register.store')
+  router.post('reset-password', [ResetPasswordsController, 'attempt']).as('reset-password.attempt')
+  router.post('forgot-password', [ForgotPasswordsController, 'attempt']).as('forgot-password.attempt')
+}).use(throttle) // apply throttle/limiter middleware
 
 router.group(() => {
   router.get('dashboard', ({ view }) => {
